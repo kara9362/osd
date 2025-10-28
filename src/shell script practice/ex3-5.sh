5fcf1ff77730c90601818aac4eebfce954ba6746
kara9362 <162804238+kara9362@users.noreply.github.com>
2025-10-28 13:23:27 +0900

    Update ex3-5.sh

--- src/shell script practice/ex3-5.sh
+++ src/shell script practice/ex3-5.sh
@@ -3,13 +3,12 @@
 run_linux_command() {
     # 첫 번째 인자는 명령어, 나머지 인자는 옵션으로 처리
     local command_name="$1"
-    shift # 명령어 이름을 제거하고 옵션만 남김
+    shift # 옵션만 남기기
     local options="$@"
 
     echo "명령어 실행: $command_name $options"
     
-    # 'eval'을 사용하여 명령어와 옵션을 조합하여 실행
-    # 주의: eval은 보안상 위험할 수 있으므로, 신뢰할 수 있는 입력에만 사용해야 합니다.
+    # 명령어와 옵션을 실행
     eval "$command_name $options"
     
     echo "------------------------------"
